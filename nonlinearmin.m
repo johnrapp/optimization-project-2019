dccd003138931ede0a7854087f46ea13b3712c33
John Rapp <johnfarnes@hotmail.com>
2019-01-17 12:51:24 +0100

    BFGS works!

--- nonlinearmin.m
+++ nonlinearmin.m
@@ -8,14 +8,14 @@ end
 
 n = numel(start);
 
-x_next = start;
+x = start;
 
 No_of_iterations = 0;
 
-for k = 1:1
+while 1
 No_of_iterations = No_of_iterations + 1;
 
-y = x_next;
+y = x;
 D = eye(n);
 for j = 1:n
     d = -D*grad(f, y);
@@ -24,13 +24,17 @@ for j = 1:n
     D = update_matrix(f, D, lambda, d, y, next_y);
     
     y = next_y;
-    x_next = next_y;
 end
+    x_next = next_y;
+    
+    if should_stop(f, x, x_next, tol)
+        break;
+    end
+    x = x_next;
 end
 
 x = x_next;
-
-if isnan(func(x)) || func(x) > func(start)
+if isnan(f(x)) || f(x) > f(start)
     error('Bad job of the search!')
 end
 
@@ -46,7 +50,12 @@ function D = update_DFP(f, D, lambda, d, y, next_y)
 	[p, q] = get_pq(f, lambda, d, y, next_y);
     D = D + (p*p')/(p'*q) - (D*(q*q')*D)/(q'*D*q);
 end
+
 function D = update_BFGS(f, D, lambda, d, y, next_y)
 	[p, q] = get_pq(f, lambda, d, y, next_y);
     D = D + (1 + q'*D*q/(p'*q))*(p*p')/(p'*q)-(p*q'*D+D*q*p')/(p'*q);
+end
+
+function stop = should_stop(f, x, x_next, tol)
+    stop = all(abs(x_next - x) <= tol) || abs(f(x_next) - f(x)) < tol;
 end
--- test_nonlinearmin.m
+++ test_nonlinearmin.m
@@ -1,9 +1,16 @@
 %% Test  DFP
 
-func = @(x) x'*[3, 2; 0, 1]*x + [1,2]*x;
+H = [3, 2; 1, 1]; c = [1,2];
+
+func = @(x) x'*H*x + c*x;
 
 nonlinearmin(func,[1;2],'DFP',0.1,1)
 
 
-%% Test  DFP
-nonlinearmin(@func,[1;2;3;4],'DFP',0.1,1)
+%% Test  BFGS
+
+H = [3, 2; 1, 1]; c = [1,2];
+
+func = @(x) x'*H*x + c*x;
+
+nonlinearmin(func,[1;2],'BFGS',0.1,1)
