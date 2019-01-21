a8e2f997822d2840832ba7aa756e5b1a8b45d69c
John Rapp <johnfarnes@hotmail.com>
2019-01-21 11:45:03 +0100

    Improved stop criterion of line search

--- test_linesearch.m
+++ test_linesearch.m
@@ -1,7 +1,7 @@
 %% Test 1
 
-%a=-10; % try a = 2, a = -2 too, then a = 5 and -5, then a = 10 and -10
-%f=@(x) (1 - 10^a*x).^2;
+a=2; % try a = 2, a = -2 too, then a = 5 and -5, then a = 10 and -10
+f=@(x) (1 - 10^a*x).^2;
 
 a_0 = -1000;
 b_0 = 1000;
@@ -19,4 +19,11 @@ hold off
 
 %% Test 2
 
-lambda_1=linesearch(@test_func,[0;0],[1;0]);
+lambda_1 = linesearch(@test_func,[0;0],[1;0])
+test_func([0;0] + lambda_1*[1;0])
+lambda_2 = linesearch(@test_func,[0;0],[0;1])
+test_func([0;0] + lambda_2*[0;1])
+
+
+lambda_3 = linesearch(@test_func,[lambda_1;0],[0;1])
+test_func([lambda_1;0] + lambda_3*[0;1])
--- test_nonlinearmin.m
+++ test_nonlinearmin.m
@@ -1,4 +1,4 @@
-%% Test  DFP
+%% Test DFP quadratic
 
 H = [3, 2; 1, 1]; c = [1,2];
 
@@ -7,10 +7,15 @@ func = @(x) x'*H*x + c*x;
 nonlinearmin(func,[1;2],'DFP',0.1,1)
 
 
-%% Test  BFGS
+%% Test BFGS quadratic
 
 H = [3, 2; 1, 1]; c = [1,2];
 
 func = @(x) x'*H*x + c*x;
 
 nonlinearmin(func,[1;2],'BFGS',0.1,1)
+
+%% Test DFP Rosenbrock
+
+func = @rosenbrock;
+nonlinearmin(func,[0;0],'DFP',1e-6,0)
