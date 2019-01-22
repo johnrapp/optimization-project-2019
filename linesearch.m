0ad72a06d823416a056393039e80ba1a318cc207
John Rapp <johnfarnes@hotmail.com>
2019-01-22 13:43:36 +0100

    Optimizing linesearch

--- linesearch.m
+++ linesearch.m
@@ -1,6 +1,6 @@
 function [lambda, No_of_iterations] = linesearch(func, x, d)
 
-    f = @(lambda) func(x + lambda * d);
+    f = @(lambda) func(x + lambda .* d);
     
     [lambda, No_of_iterations] = armijo(f);
     
@@ -13,20 +13,21 @@ end
 
 function [lambda, No_of_iterations] = armijo(F)
 
-    beta = 2;
-    epsilon = 0.1;
+    alpha = 3;
+    epsilon = 0.2;
     
     lambda_0 = 1;
-    alpha = 2;
+    beta = 1.5;
     
     lambda = lambda_0;
 
     F_0 = F(0);
     derivative_0 = derivative(F, 0);
 
-    %if (derivative_0 > 0)
+    if (derivative_0 > 0)
     %    derivative_0 = 0;
-    %end
+        error("positive derivative");
+    end
 
     T = @(lambda) F_0 + epsilon*lambda*derivative_0;
 
@@ -48,7 +49,7 @@ function [lambda, No_of_iterations] = armijo(F)
             lambda = lambda * beta;
         end
         
-        if No_of_iterations > 1000
+        if No_of_iterations > 5000
             error('Too long time in line search!')
         end
 
@@ -60,3 +61,69 @@ function dy = derivative(F, x)
     h = 1.e-60;
     dy = (F(x+h) - F(x-h))/(2*h);
 end
+
+% function [lambda, No_of_iterations] = linesearch(func,x,d)
+%  
+% %The one-dimensional function defined by the search direction d
+% F=@(lambda)func(x+lambda*d);
+%  
+% %Parameters 0 < epsilon < 1 and alpha > 1 used in Armijo's rule
+% epsilon = 0.1;
+% alpha = 2;
+%  
+% %Initial step length
+% lambda = 1; 
+%  
+% %Calculates F(0) only once to save time 
+% F0 = F(0); 
+%  
+% %Definition of derivative
+% df=@(h)(F(h)-F(-h))/2/h;
+%  
+% %Find a reasonable h to calculate F'(0) by finding an 
+% % approximation of lambda that satisfy F(lambda)=F(0)
+%  
+% No_of_iterations=0;
+% maxIters = 1000; %To prevent the algorithm from getting stuck in a loop
+%  
+% while isnan(F(lambda)) || F(lambda)>F0 && No_of_iterations<= maxIters
+%    lambda = lambda/alpha;
+%    No_of_iterations = No_of_iterations+1; 
+% end
+%  
+% while F(lambda) <= F0 && No_of_iterations<= maxIters
+%     lambda=lambda*alpha;
+%     No_of_iterations=No_of_iterations+1;
+% end
+%  
+% h=1e-6*lambda; % h is a small multiple of lambda
+% Fprime0=df(h); % calculates F'(0) using the previously defined function
+%  
+% %Condition to make sure the derivative is not positive
+% if Fprime0 > 0
+%     Fprime0 = 0;
+% end
+%  
+% %The function defined in Armijo's rule
+% T=@(lambda)F0 + epsilon*lambda*Fprime0;
+%  
+% 
+%  
+% %First condition in Armijo's rule using lambda from the previous 
+% %approximation as initial step length 
+% while F(lambda) > T(lambda) && No_of_iterations<= maxIters  
+%     lambda = lambda/alpha;
+%     No_of_iterations = No_of_iterations+1;
+% end
+%  
+% %Second condition in Armijo's rule
+% while F(alpha*lambda) < T(alpha*lambda) && No_of_iterations<= maxIters
+%     lambda = lambda*alpha;
+%     No_of_iterations = No_of_iterations+1;
+% end
+%  
+% if isnan(func(x+lambda*d)) || func(x+lambda*d)>func(x)
+%  error('Bad job of the line search!')
+% end
+%  
+% end
--- nonlinearmin.m
+++ nonlinearmin.m
@@ -18,30 +18,38 @@ x = start;
 No_of_iterations = 0;
 
 while 1
-No_of_iterations = No_of_iterations + 1;
+    No_of_iterations = No_of_iterations + 1;
 
-y = x;
-D = eye(n);
-for j = 1:n
-    d = -D*grad(f, y);
-    lambda = linesearch(f, y, d);
-    next_y = y + lambda*d;
-    D = update_matrix(f, D, lambda, d, y, next_y);
-    
-    y = next_y;
-end
+    y = x;
+    D = eye(n);
+    for j = 1:n
+        d = -D*grad(f, y);
+        lambda = linesearch(f, y, d);
+        next_y = y + lambda*d;
+        D = update_matrix(f, D, lambda, d, y, next_y);
+
+        y = next_y;
+    end
     x_next = next_y;
     
     if should_stop(f, x, x_next, tol)
         break;
     end
     x = x_next;
+    %xs(No_of_iterations) = x(1);
+    %ys(No_of_iterations) = x(2);
+    
+    if No_of_iterations > 5000
+        error('Too long time in minimization!')
+    end
 end
 
 x = x_next;
 if isnan(f(x)) || f(x) > f(start)
     error('Bad job of the search!')
 end
+
+%plot(xs, ys, '-o');
 
 %indiana_jones();
 
@@ -62,5 +70,5 @@ function D = update_BFGS(f, D, lambda, d, y, next_y)
 end
 
 function stop = should_stop(f, x, x_next, tol)
-    stop = all(abs(x_next - x) <= tol) || abs(f(x_next) - f(x)) < tol;
+    stop = all(abs(x_next - x) <= tol) || abs(f(x_next) - f(x)) < tol;;
 end
