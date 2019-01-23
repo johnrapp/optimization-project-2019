c306b1b4b0ea180ddbab6f36f0153b1bb703cb02
John Rapp <johnfarnes@hotmail.com>
2019-01-23 15:44:27 +0100

    PROJECT 2.0 - we killed it

--- test_nonlinearmin.m
+++ test_nonlinearmin.m
@@ -4,7 +4,7 @@ H = [3, 2; 1, 1]; c = [1,2];
 
 func = @(x) x'*H*x + c*x;
 
-x = nonlinearmin(func,[1;2],'DFP',0.1,1)
+[x, No_of_iterations] = nonlinearmin(func,[1;2],'DFP',0.1,1)
 func(x)
 
 
@@ -14,7 +14,7 @@ H = [3, 2; 1, 1]; c = [1,2];
 
 func = @(x) x'*H*x + c*x;
 
-x = nonlinearmin(func,[1;2],'BFGS',0.1,1)
+[x, No_of_iterations] = nonlinearmin(func,[1;2],'BFGS',0.1,1)
 func(x)
 
 %% Test DFP Rosenbrock
@@ -23,16 +23,16 @@ func = @rosenbrock;
 method = 'DFP';
 
 [x, No_of_iterations] = nonlinearmin(func,[0;0],method,1e-6,0)
-%func(x)
+func(x)
 
 [x, No_of_iterations] = nonlinearmin(func,[200;200],method,1e-6,0)
-%func(x)
+func(x)
 
 [x, No_of_iterations] = nonlinearmin(func,[-100;200],method,1e-6,0)
-%func(x)
+func(x)
 
 [x, No_of_iterations] = nonlinearmin(func,[-10;0.00001],method,1e-6,0)
-%func(x)
+func(x)
 
 %% Test BFGS Rosenbrock
 
--- test_penalty.m
+++ test_penalty.m
@@ -1,24 +1,3 @@
-%% Test 1
-
-f = @(x) x(1)^2 + x(2)^2 + x(3)^2 + x(4)^2 +x(5)^2;
-
-%g_k{1} = @(x) -x(1);
-%g_k{2} = @(x) -x(2);
-g_k = {
-    @(x) -x(1)
-    @(x) -x(2)
-}
-
-h_k{1} = @(x) x(1)^2 + x(2)^2 - 1;
-
-
-mu = 1;
-
-q = penalty(f, g_k, h_k, mu);
-
-q([1/sqrt(2);1/sqrt(2)])
-
-
 %% min exp(...)
 
 f = @(x) exp(x(1) * x(2) * x(3) * x(4) * x(5));
@@ -29,7 +8,7 @@ h_k{1} = @(x) x(1)^2 + x(2)^2 + x(3)^2 + x(4)^2 +x(5)^2 - 10;
 h_k{2} = @(x) x(2) * x(3) - 5 * x(4) * x(5);   
 h_k{3} = @(x) x(1)^3 + x(3)^3 + 1;
 
-mu = 0.011;
+mu = 0.01;
 
 x = [-2; 2; 2; -1; -1];
 iterations = 0;
@@ -58,8 +37,9 @@ x = [0;0];
 
 for k = 1:5
     q = penalty(f, g_k, h_k, mu);
-    [x, No_of_iterations] = nonlinearmin(q, x, "DFP", 1.0e-6, 0)
-    h_k{1}(x)
+    [x, No_of_iterations] = nonlinearmin(q, x, "DFP", 1.0e-6, 0);
     
     mu = mu * 10;
 end
+x
+f(x)
