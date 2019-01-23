d18f5e59e0140fec59ea83493bff442fa3f81188
Axel Sjöberg <axel96.sjoberg@gmail.com>
2019-01-23 11:56:30 +0100

    Working on penalty 2

--- test_penalty.m
+++ test_penalty.m
@@ -29,12 +29,12 @@ h_k{1} = @(x) x(1)^2 + x(2)^2 + x(3)^2 + x(4)^2 +x(5)^2;
 h_k{2} = @(x) x(2) * x(3) - 5 * x(4) * x(5);   
 h_k{3} = @(x) x(1)^3 + x(3)^3 + 1;
 
-mu = 0.01;
+mu = 1;
 
-start = [-2 2 2 -1 -1];
+start = [-2; 2; 2; -1; -1];
 
 %for k = 1:5 
-   q = penalty(f, g_k, h_k, mu)
+   q = penalty(f, g_k, h_k, mu);
    
    [x, No_of_iterations] = nonlinearmin(q, start, 'DFP', 1.0e-6, 0)
    
