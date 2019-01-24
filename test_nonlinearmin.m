f1b0ccffb9c9eda429ed62d1bb083a1d3d27ae08
Axel Sjöberg <axel96.sjoberg@gmail.com>
2019-01-24 19:04:35 +0100

    small update

--- test_nonlinearmin.m
+++ test_nonlinearmin.m
@@ -25,7 +25,7 @@ method = 'DFP';
 % [x, No_of_iterations] = nonlinearmin(func,[0;0],method,1e-6,0)
 % func(x)
 
-[x, No_of_iterations] = nonlinearmin(func,[200;200],method,1e-6,0)
+[x, No_of_iterations] = nonlinearmin(func,[200;200],method,1e-6,1)
 func(x)
 
 % [x, No_of_iterations] = nonlinearmin(func,[-100;200],method,1e-6,0)
